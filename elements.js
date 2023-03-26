eed970f3b78887bbc4bda3e14e18d46fd2ec91e7
BeeKay Koozie <kwibukabertrand@gmail.com>
2023-03-26 14:08:38 +0530

    Update elements.js

--- elements.js
+++ elements.js
@@ -22,4 +22,33 @@ var x = setInterval(function() {
 
 // Countdown of how many free days are left for me in the year
 var freeDays = 13 + 12; // 13 public holidays + 12 one day leaves
-var now = new
+var now = new Date();
+var endOfYear = new Date(now.getFullYear(), 11, 31, 23, 59, 59); // 11 is December
+var daysLeft = Math.round((endOfYear - now) / (1000 * 60 * 60 * 24));
+var weekendsLeft = Math.floor(daysLeft / 7) * 2; // assuming 2 weekends per week
+var remainingDays = daysLeft - weekendsLeft - freeDays;
+
+document.getElementById("countdown-to-free-days").innerHTML = "<div class='countdown'>Countdown of how many free days are left: </div>" + remainingDays + " days (including weekends)";
+
+// Countdown of how many hours left for my work in a year
+var workDays = 260; // assuming 52 weeks with 2 days off per week
+var workHours = workDays * 9; // 9 hours per work day
+var hoursLeft = workHours - (now.getHours() - 9); // assuming start work at 9 am
+
+document.getElementById("countdown-to-work-hours").innerHTML = "<div class='countdown'>Countdown of how many work hours left: </div>" + hoursLeft + " hours";
+
+// Countdown to finishing my job after a year
+var endOfJob = new Date("Apr 17, 2024 18:00:00 GMT+0530").getTime();
+
+var y = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = endOfJob - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown-to
